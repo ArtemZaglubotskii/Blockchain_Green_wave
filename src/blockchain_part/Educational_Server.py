5615f218121c8db9a0d3828c838560683ac6d151
Artem Zaglubotskii <mr_a953@mail.ru>
2019-10-27 09:45:29 +0300

    Данные о приватных ключах конвертируются в формат json и могут быть отправлены на загрузку в светофор.

--- src/blockchain_part/Educational_Server.py
+++ src/blockchain_part/Educational_Server.py
@@ -12,7 +12,7 @@ def register_traffic_light(count_of_traffic_lights):
     uuids_of_traffic_lights=[]
     for i in range(count_of_traffic_lights):
         uuids_of_traffic_lights.append(str(uuid.uuid4()))
-
+    traffic_lights_private_keys={}
     for i in range(len(uuids_of_traffic_lights)):
 
         uuid_of_tl=uuids_of_traffic_lights[i]
@@ -26,6 +26,10 @@ def register_traffic_light(count_of_traffic_lights):
         private_key= service_function.generate_private_key(uuid=uuid_of_tl,pin_code=str(code))
         address=service_function.generate_address(private_key)
 
-        print(tl_id)
+        traffic_lights_private_keys[tl_id]=private_key
+
+    with open("private_keys.json", "w") as write_file:
+        json.dump(traffic_lights_private_keys, write_file)
+
 
 register_traffic_light(10)
