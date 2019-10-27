0cf098cb3e1fa7e23a3b41cdf2cf71464229a152
Artem Zaglubotskii <mr_a953@mail.ru>
2019-10-27 08:18:53 +0300

    Создали файл setup.py для деплоя контрактов.

---
+++ src/blockchain_part/setup.py
@@ -0,0 +1,102 @@
+import argparse
+from web3 import Web3,HTTPProvider
+import json
+import time
+from src.blockchain_part import service_function
+
+network_config=open('network.json', 'r')
+rpcUrl=str(json.load(network_config)['rpcUrl'])
+
+web3 = Web3(HTTPProvider(rpcUrl))
+
+
+def key_to_value(args):
+    if (args.deploy != " "):
+        f = open('network.json', 'r')
+        private_key = str(json.load(f)['privKey'])
+        private_key_for_senders_account = private_key
+        addressUser = Web3.toChecksumAddress(
+            web3.eth.account.privateKeyToAccount('0x' + str(private_key_for_senders_account)).address)
+
+        with open("") as bin_file:
+            content = json.loads(bin_file.read())
+            bytecode = content['object']
+
+        with open("") as abi_file:
+            abi = json.loads(abi_file.read())
+        with open("") as bin_file:
+            content1 = json.loads(bin_file.read())
+            bytecode_pay = content1['object']
+
+        with open("") as abi_file:
+            abi_pay = json.loads(abi_file.read())
+
+
+
+        Contract = web3.eth.contract(abi=abi, bytecode=bytecode)
+
+        fastgasPrice = int(service_function.get_gasprice())
+        defaultGasPrice=int(service_function.get_gas())
+        if defaultGasPrice >= fastgasPrice:
+            gasPrice=defaultGasPrice
+        else:
+            gasPrice=fastgasPrice
+
+        tx = Contract.constructor().buildTransaction({
+            'from': addressUser,
+            'nonce': web3.eth.getTransactionCount(addressUser),
+            'gasPrice': gasPrice,
+            'gas': 1303052
+        })
+
+        signed_tx = web3.eth.account.signTransaction(tx, private_key_for_senders_account)
+        txId = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        #txReceipt = wait_tx_receipt(txId)
+        txReceipt1=web3.eth.waitForTransactionReceipt(txId)
+
+
+        Contract_pay = web3.eth.contract(abi=abi_pay, bytecode=bytecode_pay)
+
+        fastgasPrice = int(service_function.get_gasprice())
+        defaultGasPrice = int(service_function.get_gas())
+        if defaultGasPrice >= fastgasPrice:
+            gasPrice = defaultGasPrice
+        else:
+            gasPrice = fastgasPrice
+
+        tx_pay = Contract_pay.constructor().buildTransaction({
+            'from': addressUser,
+            'nonce': web3.eth.getTransactionCount(addressUser),
+            'gasPrice': gasPrice,
+            'gas': 992814,
+        })
+        signed_tx_pay = web3.eth.account.signTransaction(tx_pay, private_key_for_senders_account)
+        txId_pay = web3.eth.sendRawTransaction(signed_tx_pay.rawTransaction)
+        txReceipt_pay1=web3.eth.waitForTransactionReceipt(txId_pay)
+        if txReceipt_pay1['status'] == 1 and txReceipt1['status'] == 1:
+            print("KYC Registrar: " + txReceipt1['contractAddress'])
+            print("Payment Handler: " + txReceipt_pay1['contractAddress'])
+            with open('registrar.json', 'w') as filewrite:
+                data = {"registrar": {"address": Web3.toChecksumAddress(str(txReceipt1['contractAddress'])),
+                                      "startBlock": int(txReceipt1['blockNumber'])},
+                        "payments": {"address": Web3.toChecksumAddress(str(txReceipt_pay1['contractAddress'])),
+                                     "startBlock": int(txReceipt_pay1['blockNumber'])}}
+                json.dump(data, filewrite)
+
+def wait_tx_receipt(tx_hash, sleep_interval=0.5):
+    while True:
+        tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+        #print(tx_receipt)
+        if tx_receipt:
+            return tx_receipt
+        time.sleep(sleep_interval)
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--deploy', default=' ', nargs='?')
+parser.set_defaults(func=key_to_value)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    args.func(args)
--- src/runner.py
+++ src/runner.py
@@ -11,10 +11,11 @@ import asyncio
 
 try:
     sys.path.append(os.path.join(os.path.dirname(
-        __file__), '..', '..', '..', '..', "tools"))  # tutorial in tests
+        __file__), '..', '..', '..', '..', 'tools'))  # tutorial in tests
     sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(
-        os.path.dirname(__file__), "..", "..", "..")), "tools"))  # tutorial in docs
+        os.path.dirname(__file__), "..", "..", "..")), 'tools'))  # tutorial in docs
     from sumolib import checkBinary
+    import traci
 except ImportError:
     sys.exit(
         "please declare environment variable 'SUMO_HOME' as the root directory of your sumo installation (it should contain folders 'bin', 'tools' and 'docs')")
@@ -36,7 +37,7 @@ print(">> Simulation closed with status %s" % retcode)
 sys.stdout.flush()
 
 # start the simulation and connect to it with the script: 
-import traci
+
 from src import parse
 
 out_file = './out.xml'
@@ -44,7 +45,10 @@ net_file = './net.net.xml'
 buckets=20
 net = parse.parse_net(net_file)
 
+
+
 step_length = 1
+
 traci.start([checkBinary('sumo-gui'), '-c',"./cross3l.sumocfg", '--step-length', str(step_length)])
 
 vehicles = []
@@ -52,31 +56,15 @@ start_vel = 5
 step = 0 
 sidVehicle = {} # vehicles that want to do side move
 
+
+
 # Run a simulation until all vehicles have arrived
 while traci.simulation.getMinExpectedNumber() > 0:
     traci.simulationStep()
-    # step2 of side move
-    '''for v in sidVehicle:
-        traci.vehicle.moveTo(v,sidVehicle[v][0],sidVehicle[v][1]+8)
-    sidVehicle.clear()
 
-    for e in net:
-        vehicles = traci.edge.getLastStepVehicleIDs(e)
-        # change the Aggressive vehicles' speed mode---break traffic light
-        for v in vehicles:
-            lane = traci.vehicle.getLaneID(str(v))
-            Id = traci.vehicle.getTypeID(v)
-            print(v + ":  " + str(traci.vehicle.getSpeed(v)))
 
-            # step1 of side move
-        for l in net[e]:
-            length = traci.lane.getLength(l)
-            if traci.lane.getLastStepHaltingNumber(l) >= 2:
-                for v1 in traci.lane.getLastStepVehicleIDs(l):
-                    pos = traci.vehicle.getLanePosition(str(v1))'''
+
     step += 1
 
-
-
-print(vehicles)
+print(net)
 traci.close()
